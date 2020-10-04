4b63c0c215dcb37ef95c2cb7d4cacfb47b3f46a8
ChristopherZinger <krzysztof.zinger@gmail.com>
2020-10-04 16:02:13 +0200

    add get cv controllers

---
+++ cv/controllers/CVControllers.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { CVModel } = require('../models/CVModels');
+
+
+module.exports.saveCV = async (req, res) => {
+}
+
+
+module.exports.getCV = async (req, res) => {
+    // check for user
+    if (!res.user) return res.sendStatus(401) // not authorized
+
+    try {
+        // check if user has a education model
+        const cvList = await CVModel.find({ user: res.user._id });
+        return res.status(200).json(cvList);
+
+    } catch (err) {
+        console.log(err)
+        return res.sendStatus(500)
+    }
+}
+
+
+module.exports.removeCV = async (req, res) => {
+}
+
